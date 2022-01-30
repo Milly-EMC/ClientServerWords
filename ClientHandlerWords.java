4989b338275825ca1e155815d2db3724cae9929b
Milly-EMC <78649922+Milly-EMC@users.noreply.github.com>
2022-01-30 09:20:45 +0200

    Modified the list's instantiation place.

--- ClientHandlerWords.java
+++ ClientHandlerWords.java
@@ -5,25 +5,25 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.net.SocketException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ClientHandlerWords extends Thread {
     private final Socket socket;
-    private static List<String> receivedWords;
+    private static List<String> receivedWords = new ArrayList<>();
 
     public ClientHandlerWords(Socket socket) {
         this.socket = socket;
-
     }
 
     @Override
     public void run() {
 
-        try (BufferedReader incomingMessages = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        try (BufferedReader incomingMessages = new BufferedReader(
+                new InputStreamReader(socket.getInputStream()));
              PrintWriter outputMessages = new PrintWriter(socket.getOutputStream(), true)) {
 
-            receivedWords = new ArrayList<>();
             String receivedMessage;
 
             while (true) {
@@ -46,7 +46,7 @@ public class ClientHandlerWords extends Thread {
                     }
 
                 } catch (IOException e) {
-                    e.printStackTrace();
+                    e.getMessage();
                 }
 
             }
